9e85f57c8afe8c1f5278baa24d90cb63a573d61f
Fredrick <fredokoririck@gmail.com>
2022-08-17 21:11:12 +0300

    Changed routes

--- server.js
+++ server.js
@@ -10,28 +10,18 @@ server.use(express.static(path.join(__dirname, 'public')));
 
 server.set('port', process.env.PORT || 8080);
 
-server.get('/', (req, res) => { res.sendFile('/index.html', { root: __dirname }); });
+server.get('/', (req, res) => {
+    res.sendFile('/index.html', { root: __dirname });
+});
 
 server.get('/about', (req, res) => {
-    res.sendFile('/pages/about.html', {
+    res.sendFile('/about.html', {
         root: __dirname
     });
 });
 
-server.get('/contact', (req, res) => { res.sendFile('/pages/contact.html', { root: __dirname }); });
-
-
-server.get('/advisory', (req, res) => {
-    res.sendFile('/pages/advisory.html', {
-        root: __dirname
-    });
-});
-server.get('/governors', (req, res) => {
-    res.sendFile('/pages/governors.html', {
-        root: __dirname
-    });
-});
+server.get('/contact', (req, res) => { res.sendFile('/contact.html', { root: __dirname }); });
 
 server.listen(8080, () => {
-    console.log('Listening on port 8080');
+    console.log('Express server started at port 8080');
 });
