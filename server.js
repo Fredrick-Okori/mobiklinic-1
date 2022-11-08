89a108dc420c383c42ddf73f22afbdbe664e7991
Alpha49912 <fredokoririck@gmail.com>
2022-11-09 00:47:07 +0300

    added contact us page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'),
     server = express();
 const path = require('path');
-const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
+
+
+const port = process.env.PORT;
+server.set('port', process.env.PORT || 3000);
 
 
 server.use(express.urlencoded({ extended: true }));
@@ -20,16 +25,55 @@ server.get('/about', (req, res) => {
     });
 });
 
-server.get('/contact', (req, res) => {
-    res.sendFile('/contact.html', { root: __dirname });
-});
 
-server.get('/privary', (req, res) => {
+server.get('/privacy', (req, res) => {
     res.sendFile('/privacy.html', {
         root: __dirname
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Express server started at port ${PORT}`);
+server.get('/contact', (req, res) => {
+    res.sendFile('/contact.html', { root: __dirname });
+});
+
+
+
+server.get('/contact', (req, res) => {
+    res.sendFile('/contact.html', { root: __dirname });
+});
+
+const contactAddress = 'mobiklinicuganda@gmail.com';
+
+
+server.post('/contact', async (req, res, next) => {
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "mobiklinicuganda@gmail.com",
+            pass: "ptsayqzvfrwbwyoa",           
+        },
+    });
+    const mailOption = {
+        //build email option
+        to: [req.body.email],
+        from: `${req.body.name}`,
+        subject: `${req.body.subject}`,
+        text: req.body.message
+    }
+
+    //send mail
+    transporter.sendMail(mailOption, (req, res, next, error, info) => {
+        if (error) {
+            console.log(error);
+            res.json({ msg: 'fail' });
+        } else {
+            console.log('good')
+            res.sendFile('/contact', { root: __dirname });
+        }
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Express server started at port ${port}`);
 });
